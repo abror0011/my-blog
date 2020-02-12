5c14c75f9d6857cbb1bf9f9f9862a397e6c01837
Abror <abrororinov360@gmail.com>
2020-02-12 14:49:37 +0500

    Site layouts

---
+++ layouts/footer.php
@@ -0,0 +1,9 @@
+        <footer class="text-center">
+            Mualliflik huquqi himoyalangan &copy; <?= date('Y') ?>
+        </footer>
+    </div>
+<!-- SCRIPTS -->
+<script src="assets/js/jquery.js"></script>
+    <script src="assets/js/bootstrap.js"></script>
+</body>
+</html>
